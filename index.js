3ffaf6f5368b9b1392c9d16f81f411058acfb3d0
vincent <vinnymummo@gmail.com>
2024-04-11 10:16:30 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body')
+function submitData(name, email){
+    const newData = {
+        name:"Steve",
+        email:"steve@steve.com",
+    }
+return fetch("http://localhost:3000/users",{
+    method:"POST",
+    headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(newData)
+})
+.then(res => res.json())
+.then (data =>{
+    document.body.innerHTML= data["id"]
+})
+.catch(error =>{
+    const err = document.createElement('div')
+    err.innerHTML ="Unauthorized Access"
+    body.append(err)
+})
+}
+
 // Add your code here
